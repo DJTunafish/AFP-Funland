967c8e031f5720308046043f5b5e6093c70e6791
DJTunafish <jungmark.erik@gmail.com>
2015-02-25 16:05:35 +0100

    Fixed Examples.hs

--- Examples.hs
+++ Examples.hs
@@ -16,31 +16,31 @@ main = runProgram $ parallelHeart 150
 spiral :: Double -> Double -> Program
 spiral size angle =
   case size > 100 of
-    True  -> [die]
+    True  -> die
     False ->
-      concat [[forward size, right angle], prog]
+      forward size >*> right angle >*> prog
       where prog = spiral (size+2) angle
 
 -- |'spiralInfinite' returns a program that performs forward
 -- and right sequentially and never stops
 spiralInfinite :: Double -> Double -> Program
 spiralInfinite size angle =
-  concat [[forward size, right angle], prog]
+  forward size >*> right angle >*> prog
   where prog = spiralInfinite (size+2) angle
 
 -- |'square' draws a square
 square :: Double -> Program
-square size = [times 4 [forward size, right 90], die]
+square size = (times 4 (forward size >*> right 90)) >*> die
 
 -- |'star' draws a star
 star :: Double -> Program
-star size = [times 5 [forward size, right 144], die]
+star size = (times 5 (forward size >*> right 144)) >*> die
 
 -- |'parallelHeart' draws a heart in parallel
 parallelHeart :: Double -> Program
 parallelHeart size =
-  [penUp, backward size, penDown] ++ (leftHalf <|> rightHalf)
-  where leftHalf  = [left 45, forward size, right 90
-                    , forward (size/2), right 90, forward (size/2), die]
-        rightHalf = [right 45, forward size, left 90
-                    , forward (size/2), left 90, forward (size/2), die]
+  penUp >*> backward size >*> penDown >*> (leftHalf <|> rightHalf)
+  where leftHalf  = left 45 >*> forward size >*> right 90 >*>
+                    forward (size/2) >*> right 90 >*> forward (size/2) >*> die
+        rightHalf = right 45 >*> forward size >*> left 90 >*>
+                    forward (size/2) >*> left 90 >*> forward (size/2) >*> die
