e33d8bfb15b6b271a1b1146d8f89d6d0b6c7b563
DJTunafish <jungmark.erik@gmail.com>
2015-02-25 15:55:51 +0100

    Pray that the turtle works

--- Turtle.hs
+++ Turtle.hs
@@ -53,12 +53,14 @@ data Action where
   Color      :: Color   -> Action
   Die        :: Action
   Idle       :: Action
-  Limited    :: Int     -> Program -> Action
-  Lifespan   :: Int     -> Program -> Action
-  Times      :: Int     -> Program -> Action
-  Forever    :: Program -> Action
-  Sequential :: Action  -> Action  -> Action
+  StartDeathClock :: Int -> Action
   Parallel   :: Program -> Program -> Action
+ {- Limited    :: Int     -> Program -> Program
+  Lifespan   :: Int     -> Program -> Program
+  Times      :: Int     -> Program -> Program
+  Forever    :: Program -> Program
+  Sequential :: Program  -> Program  -> Program
+  Parallel   :: Program -> Program -> Program-}
   deriving Show --for testing purposes
 
 -- The 'Turtle' data type consists of six attributes
@@ -82,47 +84,52 @@ data Turtle =
 -------------------------------------------
 
 -- | Returns an action that moves the turtle n steps forward
-forward = Forward
+forward x = [Forward x]
 -- | Returns an action that moves the turtle n steps backward
-backward = Backward
+backward x = [Backward x]
 -- | Returns an action that rotates the turtle n degrees right
-right = TRight
+right x = [TRight x]
 -- | Returns an action that rotates the turtle n degrees left
-left = TLeft
+left x = [TLeft x]
 -- | Returns an action that disables the turtle from drawing
 --   anything
-penUp = PenUp
+penUp = [PenUp]
 -- | Returns an action that enables the turtle to draw
-penDown = PenDown
+penDown = [PenDown]
 -- | Returns an action that changes the color of the turtles 
 --   pen to the given color
-color = Color
+color c = [Color c]
 -- | Returns an action that "kills" the turtle, so that
 --   it will ignore any instructions from future instructions
-die = Die
+die = [Die]
 -- | Returns an action that does nothing
-idle = Idle
+idle = [Idle]
 -- | Executes a given number of instructions from the given
 --  program
-limited = Limited
+limited n p = take n p
 -- | Executes a given number of instructions from the given
---   program, before executing the "die" action
-lifespan = Lifespan
+--   program, besfore executing the "die" action
+lifespan :: Int -> Program -> Program
+lifespan n p = [StartDeathClock n] ++ p
 -- | Returns an action that will execute the given action
 --   a given number of times
-times = Times
+times :: Int -> Program -> Program
+times n p = concat (take n (cycle [p]))
 -- | Returns an action that will endlessly execute the 
 --   given program
-forever = Forever
+forever :: Program -> Program
+forever p = cycle p 
 -- | Given two actions, returns an action that executes the first
 --   action, followed by the second.
-(>*>) = Sequential
+(>*>) :: Program -> Program -> Program
+(>*>) a b = a ++ b 
 
 -- | Given two programs, executes both programs in parallel
 (<|>) :: Program -> Program -> Program
 (<|>) p q = [Parallel p q]
 
 --p <|> q <|> r = [Parallel p q] <|> r = Parallel [Parallel p q] r
+-- [Parallel a b] <|> [c] = [Parallel (Parallel a b) c]
 
 ----------------------------------------------
 -- run-functions and their helper functions --
@@ -134,9 +141,19 @@ runProgram p = runGraphics $ do
    w <- openWindowEx "Turtle!" Nothing (300, 300) DoubleBuffered (Just 1000)
    drawInWindow w (polygon [(0,0),(0,300),(300,300),(300,0)]) 
    onTick w p turtleInit
+   return ()
+
+onTick :: Window -> Program -> Turtle -> IO ()
+onTick w [] _ = return ()
+onTick w p t = case life t' of
+                False -> return ()
+                True  -> do t'' <- runAction (head p) t' w
+                            onTick w (tail p) t''
+  where t' = deathAndTaxes t
 
 -- 'onTick' runs one action at a time if the Turtle is alive
 -- If the next action is a parallel execution, 'runParallel' will handle it
+{-
 onTick :: Window -> Program -> Turtle -> IO ()
 onTick _w [] _t    = return ()
 onTick w (p:ps) t  =
@@ -154,6 +171,7 @@ onTick w (p:ps) t  =
             putStr "\n"
             onTick w (p' ++ ps) t2
   where t' = deathAndTaxes t
+-}
 
 -- 'deathAndTaxes' counts down a Turtle's 'deathclock'
 -- and kills it when its time has come
@@ -167,6 +185,7 @@ deathAndTaxes t =
 
 -- 'runParallel' executes multiple Programs in parallel
 -- with the help of runParallelAction
+{-
 runParallel :: Window -> [(Program, Turtle)] -> IO ()
 runParallel w [] = return () 
 runParallel w pt = do
@@ -191,14 +210,14 @@ runParallelAction w (p, t)                   =
         return $ [((p' ++ tail p), t'')]
         where
           t' = deathAndTaxes t
-
+-}
 -- A turtle with standard values
 turtleInit :: Turtle
 turtleInit =
   Ttl { location = (150,150), orientation = 270, life = True
       , toggleDraw = True, penColor = Black, deathclock = -1}
 
-
+{-
 -- 'interpretProgram' takes a program and pattern matches for the actions
 -- 'Limited', 'Forever', 'Sequential', and 'Times'. If any of these cases
 -- are found the function converts them to programs without these constructors
@@ -215,8 +234,11 @@ interpretProgram ((Times n p):ps)      =
 interpretProgram (p:ps)                =
   [p] ++ interpretProgram ps
 
+-}
+
 -- 'runAction' executes the Action given to the turtle in a given window
-runAction :: Action -> Turtle -> Window -> IO (Program, Turtle)
+{-
+runAction :: Program -> Turtle -> Window -> IO Turtle --(Program, Turtle)
 runAction (Forward n)    t w = do
     case toggleDraw t of
         True -> do 
@@ -231,38 +253,112 @@ runAction (Forward n)    t w = do
               x2        = round $ (fromIntegral x1) + a
               y2        = round $ (fromIntegral y1) + o
               graphic   = withColor (penColor t) $ line (x1, y1) (x2, y2)
-runAction (TRight d)     t w =
-  return $ ([], t {orientation = mod' ((orientation t) + d) 360}) 
-runAction (Backward n)   t w = runAction (Forward (-n)) t w
-runAction (TLeft d)      t w = runAction (TRight (-d)) t w
-runAction (PenUp)        t w = return $ ([], t {toggleDraw = False})
-runAction (PenDown)      t w = return $ ([], t {toggleDraw = True})
-runAction (Color c)      t w = return $ ([], t {penColor = c})
-runAction (Die)          t w = return $ ([], t {life = False})
-runAction (Idle)         t w = return ([], t)
-runAction (Lifespan n p) t w =
+runAction (TRight d)     t w   = return $ 
+                                ([], t {orientation = mod' ((orientation t) + d) 360}) 
+runAction (Backward n)   t w   = runAction (Forward (-n)) t w
+runAction (TLeft d)      t w   = runAction (TRight (-d)) t w
+runAction (PenUp)        t w   = return $ ([], t {toggleDraw = False})
+runAction (PenDown)      t w   = return $ ([], t {toggleDraw = True})
+runAction (Color c)      t w   = return $ ([], t {penColor = c})
+runAction (Die)          t w   = return $ ([], t {life = False})
+runAction (Idle)         t w   = return ([], t)
+runAction (Forever p)    t w   =
+runAction (Sequential a b) t w =
+runAction (Limited n p)  t w =
+{-runAction (Lifespan n p) t w =
   if ((n >= deathclock t) && (deathclock t /= -1))
   then return (p, t)
-  else return (p, t {deathclock = n})
-runAction p                         t w = return (interpretProgram [p], t)
+  else return (p, t {deathclock = n})-}
+runAction (Lifespan n p) t w =
+runAction (Parallel a b) t w =
+--runAction p                         t w = return (interpretProgram [p], t)
+-}
 
+runAction :: Action -> Turtle -> Window -> IO Turtle --(Program, Turtle)
+runAction (Forward n)    t w = do
+    case toggleDraw t of
+        True -> do 
+                  getWindowTick w
+                  drawInWindow w graphic
+                  return (t {location = (x2, y2)})
+        False -> return (t {location = (x2, y2)})         
+        where
+              a         = n * cos (((orientation t)/180) * pi)
+              o         = n * sin (((orientation t)/180) * pi)
+              (x1, y1)  = location t
+              x2        = round $ (fromIntegral x1) + a
+              y2        = round $ (fromIntegral y1) + o
+              graphic   = withColor (penColor t) $ line (x1, y1) (x2, y2)
+runAction (TRight d)     t w   = return (t {orientation = mod' ((orientation t) + d) 360})  
+runAction (Backward n)   t w   = runAction (Forward (-n)) t w
+runAction (TLeft d)      t w   = runAction (TRight (-d)) t w
+runAction (PenUp)        t w   = return (t {toggleDraw = False})
+runAction (PenDown)      t w   = return (t {toggleDraw = True}) 
+runAction (Color c)      t w   = return (t {penColor = c})
+runAction (Die)          t w   = return (t {life = False})
+runAction (Idle)         t w   = return t
+runAction (StartDeathClock n) t w = 
+  if ((n >= deathclock t) && (deathclock t /= -1))
+  then return t
+  else return (t {deathclock = n})
+runAction (Parallel p q) t w = do runParallel w [(p, t), (q, t)]
+                                  return t
+                                       
+{-runAction ((Forever p):ps)    t w   = 
+runAction ((Sequential a b):ps) t w = do t' <- runAction a t w
+                                    runAction b t' w
+runAction ((Limited n p):ps)  t w   = 
+{-runAction (Lifespan n p) t w =
+  if ((n >= deathclock t) && (deathclock t /= -1))
+  then return (p, t)
+  else return (p, t {deathclock = n})-}
+runAction (Lifespan n p) t w =
+runAction (Parallel a b) t w =
+-}
+
+runParallel :: Window -> [(Program, Turtle)] -> IO ()
+runParallel w [] = return () 
+runParallel w pt = do
+  --putStrLn "Following actions performed in parallel: "
+  pt' <- mapM (runParallelAction w) pt
+  runParallel w (concat pt')
+                    
+-- 'runParallelAction' executes multiple 'Program's in parallel
+-- by one Action per 'Program' each
+runParallelAction :: Window -> (Program, Turtle) -> IO [(Program, Turtle)] 
+runParallelAction _w ([], t)                        = return []
+runParallelAction w (((Parallel p q):ps), t) = return [(ps, t), (p, t), (q, t)]
+runParallelAction w (p, t)                   =
+  case life t of
+    False -> return []
+    True ->
+      do
+        t'' <- runAction (head p) t' w
+    --    putStr "  "
+    --    printAction (head p)
+    --    putStr "\n"
+        return $ [(((tail p) ++ tail p), t'')]
+        where
+          t' = deathAndTaxes t
+          
+      
 printAction :: Action -> IO ()
 printAction (Forward n)      = putStr $ "forward " ++ show n
 printAction (TRight n)       = putStr $ "right " ++ show n
 printAction (TLeft n)        = putStr $ "left " ++ show n
 printAction (Backward n)     = putStr $ "backward " ++ show n
-printAction (Limited n p)    = do putStr $ "Limited " ++ show n ++ " ["
+{-printAction (Limited n p)    = do putStr $ "Limited " ++ show n ++ " ["
                                   printAction (head p)
                                   putStr " .. ]" 
 printAction (Lifespan n p)   = do putStr $ "Lifespan " ++ show n ++ " [" 
                                   printAction $ head p
-                                  putStr " .. ]"
+                                  putStr " .. ]"-}
 printAction (PenUp)          = putStr "penUp"
 printAction (PenDown)        = putStr "penDown"
 printAction (Die)            = putStr "Turtle soup"
 printAction (Idle)           = putStr "Idle"
 printAction (Color c)        = putStr $ "color " ++ show c
-printAction (Times n p)      = do putStr $ "times " ++ show n ++ " ["
+{-printAction (Times n p)      = do putStr $ "times " ++ show n ++ " ["
                                   printAction $ head p
                                   putStr " .. ]"
 printAction (Forever p)      = do putStr "forever [" 
@@ -278,7 +374,7 @@ printAction (Parallel p q)   = do putStr "[ "
                                   putStr "[ "
                                   printAction (head q)
                                   putStr " .. ]"
-
+-}
 
 
 
