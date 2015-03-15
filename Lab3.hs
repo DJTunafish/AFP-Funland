a291fbd1fa0692b71ba6b6285e17d0b672506e9c
DJTunafish <jungmark.erik@gmail.com>
2015-03-15 15:17:54 +0100

    askasfk

--- Lab3.hs
+++ Lab3.hs
@@ -2,58 +2,72 @@ import System.Environment (getArgs)
 import Control.Distributed.Process
 import Control.Distributed.Process.Node
 import Control.Distributed.Process.Backend.SimpleLocalnet
-import Control.Monad 
---import Data.String.Utils
 import Control.Distributed.Process.Closure
 
 chatSend :: (String, SendPort String) -> Process ()
---chatSend :: (String, ProcessId) -> Process ()
-chatSend (usrname, chan) = do self <- getSelfPid
-                              liftIO $ putStrLn ("Ready to send to " ++ (show chan))
-                              msg <- liftIO getLine
-                              sendChan chan (usrname ++ ">" ++ msg)
-                              --send pid (usrname ++ ">" ++ msg)
-                              (s, r) <- newChan :: Process (SendPort String, ReceivePort String)
-                     --         liftIO $ putStrLn ("Sent message to " ++ (show s))
-                              chatSend (usrname, chan)
+chatSend x = do Just monitor <- whereis "control"
+                link monitor 
+                chatSend' x
 
-chatReceive :: (ReceivePort String) -> Process ()
-chatReceive chan = do self <- getSelfPid
-                      liftIO $ putStrLn $ "Ready to receive at process " ++ (show self)
-                      msg <- receiveChan chan
-                 --     msg <- expect :: Process String
-                      liftIO $ putStrLn "GOOOOOOOD DAMN IT"
-                      liftIO $ putStrLn msg
-                      chatReceive chan
+chatSend' :: (String, SendPort String) -> Process ()
+chatSend' (usrname, chan) = do msg <- liftIO getLine
+                               case msg of 
+                                "/disconnect" -> do Just monitor <- whereis "control"
+                                                    sendChan chan "/disconnect"
+                                                    liftIO $ putStrLn "Disconnecting from chat..."
+                                                    send monitor "EVERYONE DIES"
+                                _             -> do sendChan chan (usrname ++ ">" ++ msg)
+                                                    chatSend' (usrname, chan)
 
-chatSetup :: ProcessId -> Process ()
-chatSetup pid = do liftIO $ putStrLn "Setting up chat..."
-                   (sPort, rPort) <- newChan :: Process (SendPort String, ReceivePort String)
-             --      self <- getSelfPid
-                   spawnLocal (chatReceive rPort)
-                   --spawnLocal chatReceive
-                   send pid sPort
-                --   send pid self
-               
+roomSend :: (String, SendPort (String, NodeId)) -> Process ()
+roomSend x = do Just monitor <- whereis "control"
+                link monitor
+                roomSend' x
 
-initClient :: ProcessId -> Process ()
-initClient pid = do 
-                 liftIO $ putStrLn greetMessage
-                 usrname <- liftIO getLine
-                 self <- getSelfNode
-                -- send pid ("/newUser", [usrname], self)
-                 liftIO $ putStrLn "Would you like to join a chatroom, contact a user, or wait for someone"
-               --         "to contact you? Enter \"passive\" to wait, \"room\" to connect to a chatroom" ++ 
-               --         "and \"contact\" to contact a user.")
-                 choice <- liftIO getLine
-                 case choice of
-                    "passive" -> --do self <- getSelfNode
-                                 send pid ("/newUser", [usrname], self)
-                    "contact" -> do liftIO $ putStrLn "Please enter the name of the user you wish to chat with"
-                                    otherUsr <- liftIO getLine
-                                    send pid ("/startChat", [usrname, otherUsr], self)
-                    "chatroom" -> undefined                
-                 
+roomSend' :: (String, SendPort (String, NodeId)) -> Process ()
+roomSend' (usrname, chan) = do self <- getSelfNode
+                               msg <- liftIO getLine
+                               sendChan chan ((usrname ++ ">" ++ msg), self)
+                               roomSend' (usrname, chan)
+
+chatReceive :: () -> ReceivePort String -> Process ()
+chatReceive () chan = do Just monitor <- whereis "control"
+                         link monitor
+                         chatReceive' chan
+
+chatReceive' :: ReceivePort String -> Process ()
+chatReceive' chan = do self <- getSelfPid
+                       msg <- receiveChan chan
+                       case msg of
+                        "/disconnect" -> do Just monitor <- whereis "control"
+                                            liftIO $ putStrLn "Other user disconnected, ending chat..."
+                                            send monitor "EVERYONE DIES"
+                        _             -> do liftIO $ putStrLn msg
+                                            chatReceive' chan
+
+                       
+serverMessage :: String -> Process ()
+serverMessage s = liftIO $ putStrLn s           
+              
+
+waitForInput :: (String, ProcessId) -> Process () 
+waitForInput (msg, pid) = do 
+    liftIO $ putStrLn msg
+    input <- liftIO $ getLine 
+    self <- getSelfNode
+    let args  = words input
+    let cmd   = head args
+    let args' = tail args
+    send pid (cmd, args', self)       
+
+sdictString :: SerializableDict String
+sdictString = SerializableDict               
+
+remotable ['chatSend, 'chatReceive, 'waitForInput, 'sdictString, 'roomSend, 'serverMessage]
+
+rtable :: RemoteTable
+rtable = __remoteTable initRemoteTable 
+
 greetMessage :: String
 greetMessage = "Welcome to the Awesomely Radical chat client! \n" ++
                "Please start by connecting to a user or chat room. \n" ++
@@ -62,27 +76,18 @@ greetMessage = "Welcome to the Awesomely Radical chat client! \n" ++
                "For other commands, please enter /help \n \n" ++
                "Please enter a username"
 
+initMessage :: String 
+initMessage = "Welcome to the Awesomely Radical chat client! \n" ++
+              "To see a list of available commands, enter /help. \n" ++
+              "Otherwise, please enter a command now"
+
 helpMessage :: String
-helpMessage = "Poopypoopypoop" --TODO
-
-waitForInput :: (String, ProcessId) -> Process () 
-waitForInput (msg, pid) = do 
-    liftIO $ putStr msg
-    input <- liftIO $ getLine --What happens if another process runs getLine while this is in effect?
-    self <- getSelfNode
-    let args  = words input
-    let cmd   = head args
-    let args' = tail args
-    send pid (cmd, args', self)            
-
-printHelp :: Process ()
-printHelp = liftIO $ putStrLn ("/startChat: Start a chat with another user")             
-
---startGroupChat :: ProcessId -> Process ()
-remotable ['chatSend, 'chatSetup, 'initClient, 'printHelp, 'waitForInput]
-
-rtable :: RemoteTable
-rtable = __remoteTable initRemoteTable 
+helpMessage = "/startChat ownUsrName usrToContact : Start a chat with another user \n" ++
+              "/startRoom roomName userName : Start a new chatroom \n" ++
+              "/joinRoom roomName userName : Connect to an existing chatroom \n" ++
+              "/passive usrname , wait for someone to contact you \n" ++
+              "/disconnect , disconnect from current chat \n" ++
+              "/help , displays all commands"
 
 main :: IO ()
 main = do
@@ -90,16 +95,11 @@ main = do
   
   putStrLn $ show args
   case args of
-    ["server", host, port] -> do --start a chat server
-      backend <- initializeBackend host port rtable
+    ["server", host, port] -> do 
+      backend <- initializeBackend host port initRemoteTable
       startMaster backend (master backend)
     ["client", host, port] -> do
       backend <- initializeBackend host port rtable
-   {-   putStrLn ("Welcome to the Awesomely Radical chat client! \n" ++
-               "Please start by connecting to a user or chat room. \n" ++
-               "For a list of available users, enter /users \n" ++
-               "For a list of available chatrooms, enter /rooms. \n" ++
-               "For other commands, please enter /help") -} --Move this to when client has managed to connect
       node <- newLocalNode backend
       runProcess node (clientIgnition backend)
     _        -> putStrLn "Illegal command" 
@@ -112,7 +112,10 @@ clientIgnition back = do
          Nothing    -> liftIO $ putStrLn "Server down. Try again later"
          (Just pid) -> do self <- getSelfNode
                           send pid ("/connect", ([]::[String]),  self)
-                          liftIO $ startSlave back
+                          selfPid <- getSelfPid
+                          register "control" selfPid
+                          expect :: Process String
+                          return ()
 
 findServer :: Backend -> Process (Maybe ProcessId)
 findServer back = do 
@@ -126,57 +129,105 @@ findServer back = do
                              case msg of
                                 WhereIsReply _ (Just pid) -> return $ Just pid
                                 _                         -> isServer xs
---[(String, ProcessId)] = list of chat rooms [(String, NodeId)] = Users & their NodeId
+
 master :: Backend -> [NodeId] -> Process ()
 master backend slaves = do
     self <- getSelfPid
     register "server" self
     server backend [] [] slaves
 
-server :: Backend -> [(String, ProcessId)] -> [(String, NodeId)]-> [NodeId] -> Process ()
+server :: Backend -> [(String, SendPort (String, NodeId))] -> [(String, NodeId)]-> [NodeId] -> Process ()
 server backend chatRooms users slaves = do
-  --  forever $ receiveWait [match (spawnCommand backend chatRooms users), match singleChat]
     (cmd, args, nid) <- expect :: Process (String, [String], NodeId)
-    --Perhaps change this to use receiveWait and match
     self <- getSelfPid
-    case cmd of --Need an initial handshake message for setting username and such
-        "/startChat" -> let usr = head args 
-                            otherUsr = head (drop 1 args)
-                        in case findUsr otherUsr users of
+    peers <- liftIO $ findPeers backend 500
+    liftIO $ putStrLn (show peers)
+    case (cmd, args) of 
+        ("/startChat", [usr, otherUsr]) -> 
+                     case findX otherUsr users of
                             Nothing -> let msg = "No such user."
                                        in do spawn nid ($(mkClosure 'waitForInput) (msg, self))
                                              server backend chatRooms users slaves
-                            Just n  -> do spawn nid ($(mkClosure 'chatSetup) (self))
-                                          sPort1 <- expect :: Process (SendPort String)
-                                       --   sPort1 <- expect :: Process ProcessId
-                                          spawn n ($(mkClosure 'chatSetup) (self))
-                                          sPort2 <- expect :: Process (SendPort String)
-                                        --  sPort2 <- expect :: Process ProcessId
+                            Just n  -> do sPort1 <- spawnChannel $(mkStatic 'sdictString) nid ($(mkClosure 'chatReceive) (()))
+                                          sPort2 <- spawnChannel $(mkStatic 'sdictString) n ($(mkClosure 'chatReceive) (()))
                                           spawn nid ($(mkClosure 'chatSend) (usr, sPort2))
                                           spawn n ($(mkClosure 'chatSend) (otherUsr, sPort1))
+                                          spawn nid ($(mkClosure 'serverMessage) ("Now chatting with " ++ otherUsr))
+                                          spawn n ($(mkClosure 'serverMessage) ("Now chatting with " ++ usr))  
                                           server backend chatRooms users slaves
-        "/startRoom" -> undefined
-        "/joinRoom"  -> undefined
-        "/help"      -> do spawn nid ($(mkClosure 'waitForInput) (helpMessage, self))
+        ("/startRoom", [roomName, usr]) -> do 
+                           sPortC <- spawnChannel $(mkStatic 'sdictString) nid ($(mkClosure 'chatReceive) (()))
+                           sPortR <- spawnChannelLocal $ roomHandler roomName [(usr, sPortC)]
+                           spawn nid ($(mkClosure 'roomSend) (usr, sPortR))
+                           spawn nid ($(mkClosure 'serverMessage) ("Started room " ++ roomName))
+                           server backend ((roomName, sPortR):chatRooms) users slaves
+        ("/joinRoom", [room, user])  -> do 
+                           case findX room chatRooms of
+                            Nothing    -> do spawn nid ($(mkClosure 'waitForInput)("No such room", self)) 
+                                             server backend chatRooms users slaves
+                            Just sPort -> do sendChan sPort (("//addUser " ++ user), nid)
+                                             spawn nid ($(mkClosure 'roomSend) (user, sPort))
+                                             spawn nid ($(mkClosure 'serverMessage) ("Joined room " ++ room))
+                                             server backend chatRooms users slaves
+        ("/help", _)      -> do spawn nid ($(mkClosure 'waitForInput) (helpMessage, self))
+                                server backend chatRooms users slaves
+        ("/connect", _)   -> do spawn nid ($(mkClosure 'waitForInput) (initMessage, self)) 
+                                server backend chatRooms users slaves
+        ("/remRoom", [roomName]) -> server backend (remRoom roomName chatRooms) users slaves
+        ("/passive", [usr])   -> 
+                           case findX usr users of
+                             Nothing -> do spawn nid ($(mkClosure 'serverMessage) 
+                                                        ("Waiting for another user to start chat..."))
+                                           server backend chatRooms ((usr, nid):users) slaves
+                             _       -> do spawn nid ($(mkClosure 'waitForInput)("Username taken", self)) 
+                                           server backend chatRooms users slaves
+        _            -> do spawn nid ($(mkClosure 'waitForInput)("No command found, or incorrect arguments", self)) 
                            server backend chatRooms users slaves
-        "/connect"   -> do spawn nid ($(mkClosure 'initClient) (self)) 
-                           server backend chatRooms users slaves
-        "/newUser"   -> server backend chatRooms ((head args, nid):users) slaves
-        _            -> undefined
-    where findUsr :: String -> [(String, NodeId)] -> Maybe NodeId
-          findUsr x [] = Nothing
-          findUsr x ((y, n):ys) = case x == y of
-                                True  -> Just n
-                                False -> findUsr x ys 
+    where findX :: String -> [(String, a)] -> Maybe a
+          findX x [] = Nothing
+          findX x ((y, n):ys) = case x == y of
+                                 True  -> Just n
+                                 False -> findX x ys 
+          remRoom :: String -> [(String, a)] -> [(String, a)]
+          remRoom _ []          = []
+          remRoom x ((y, n):ys) = if x == y then ys else (y, n):(remRoom x ys)
 
+roomHandler :: String -> [(String, SendPort String)] -> ReceivePort (String, NodeId) -> Process ()
+roomHandler self clients rPort = do
+    (msg, nid) <- receiveChan rPort
+    let msg' = tail $ dropWhile (\a -> a /= '>' && a /= '/') msg 
+    let msgWords = words msg' 
+    case head msgWords of
+        "/addUser"    -> do  
+                         let usr = head (drop 1 msgWords)
+                         sPort <- spawnChannel $(mkStatic 'sdictString) nid ($(mkClosure 'chatReceive) (()))  
+                         mapM (\(_, chan) -> sendChan chan ("User " ++ usr ++ " joined!")) clients
+                         roomHandler self ((usr, sPort):clients) rPort
+        "/disconnect" -> do 
+                         let usr = takeWhile (\a -> a /= '>') msg
+                         let clients' = remUser usr clients
+                         liftIO $ putStrLn $ "Disconnecting user. Clients: " ++ (show clients')
+                         whereisRemoteAsync nid "control"
+                         WhereIsReply _ (Just con) <- expect :: Process WhereIsReply
+                         spawn nid ($(mkClosure 'serverMessage) ("Exiting chatroom..."))
+                         send con "8==D"
+                         mapM (\(_, chan) -> sendChan chan ("User " ++ usr ++ " left")) clients'
+                         if clients' == []
+                         then do nid <- getSelfNode
+                                 Just serv <- whereis "server"
+                                 send serv ("/remRoom", [self], nid)
+                                 return ()
+                         else roomHandler self (remUser usr clients') rPort
+        _            -> do 
+                         mapM (\(_, chan) -> sendChan chan msg) clients
+                         roomHandler self clients rPort
+  where remUser :: String -> [(String, SendPort String)] -> [(String, SendPort String)]
+        remUser _ []           = []
+        remUser s ((y, sp):ys) = if s == y
+                                 then ys
+                                 else (y, sp):(remUser s ys)
 
-                                
-{-spawnCommand :: Backend -> [(String, ProcessId)] -> [(String, NodeId)] -> (String, NodeId) -> Process ()   
-spawnCommand back chatRooms users (msg, pid) = 
-    case msg of
-        "/connect" -> spawn pid ($(mkClosure 'initClient) (self))
-        "/help"    -> spawn pid ($(mkClosure 'printHelp))
--}                                     
+                                                                              
 
 
 
