0229a5dbbbf2040990af17aad2661b90b856b461
DJTunafish <jungmark.erik@gmail.com>
2015-03-15 17:53:37 +0100

    Sluta kolla på snusk, Phillip

--- ChatClient.hs
+++ ChatClient.hs
@@ -7,11 +7,17 @@ import Control.Distributed.Process.Node
 import Control.Distributed.Process.Backend.SimpleLocalnet
 import Control.Distributed.Process.Closure
 
+--Setup for the chatSend' function. Links the process
+--to the "control" process on the local node,
+--so that when said process dies, so does this process.
 chatSend :: (String, SendPort String) -> Process ()
 chatSend x = do Just monitor <- whereis "control"
-                link monitor --Implement disconnect for regular chats
+                link monitor 
                 chatSend' x
 
+--Send function for simple client to client chat.
+--Sends messages on the given channel, which is 
+--directly connected to the other client.
 chatSend' :: (String, SendPort String) -> Process ()
 chatSend' (usrname, chan) = do msg <- liftIO getLine
                                case msg of 
@@ -22,22 +28,32 @@ chatSend' (usrname, chan) = do msg <- liftIO getLine
                                 _             -> do sendChan chan (usrname ++ ">" ++ msg)
                                                     chatSend' (usrname, chan)
 
+--Setup for the roomSend' function. Links the process
+--to the "control" process on the local node,
+--so that when said process dies, so does this process.
 roomSend :: (String, SendPort (String, NodeId)) -> Process ()
 roomSend x = do Just monitor <- whereis "control"
                 link monitor
                 roomSend' x
 
+--Send function for a chatRoom. Gets inoput from the user
+--and uses the given SendPort to send this message along 
+--with the local nodes ID to the process on the server
+--that manages the chatRoom.
 roomSend' :: (String, SendPort (String, NodeId)) -> Process ()
 roomSend' (usrname, chan) = do self <- getSelfNode
                                msg <- liftIO getLine
                                sendChan chan ((usrname ++ ">" ++ msg), self)
                                roomSend' (usrname, chan)
 
+--Setup for the chatReceive function. You know the drill.
 chatReceive :: () -> ReceivePort String -> Process ()
 chatReceive () chan = do Just monitor <- whereis "control"
                          link monitor
                          chatReceive' chan
 
+--Receive function for clients. Receives messages
+--on the given receiveport and outputs them on stdout.
 chatReceive' :: ReceivePort String -> Process ()
 chatReceive' chan = do self <- getSelfPid
                        msg <- receiveChan chan
@@ -48,11 +64,12 @@ chatReceive' chan = do self <- getSelfPid
                         _             -> do liftIO $ putStrLn msg
                                             chatReceive' chan
 
-                       
+--Used by the server to output a single message                       
 serverMessage :: String -> Process ()
 serverMessage s = liftIO $ putStrLn s           
               
-
+--Waits for input from the user and sends the given
+--message to the given ProcessId
 waitForInput :: (String, ProcessId) -> Process () 
 waitForInput (msg, pid) = do 
     liftIO $ putStrLn msg
@@ -68,9 +85,16 @@ sdictString = SerializableDict
 
 remotable ['chatSend, 'chatReceive, 'waitForInput, 'sdictString, 'roomSend, 'serverMessage]
 
+--RemoteTable containing the functions that other nodes 
+--may use to spawn processes on this node
 rtable :: RemoteTable
 rtable = __remoteTable initRemoteTable 
 
+--Starts a client on the given backend, if the server is up.
+--If the server is available, the function sends a connect
+--request to the server, then registers the current process
+--as the "control" process on this node. Other processes on 
+--this node will later link themselves to this process.
 clientIgnition :: Backend -> Process ()
 clientIgnition back = do 
       server <- findServer back
@@ -83,6 +107,9 @@ clientIgnition back = do
                           expect :: Process String
                           return ()
 
+--Attempts to find the server on the given backend
+--by searching for a process registered as "server"
+--on the available nodes
 findServer :: Backend -> Process (Maybe ProcessId)
 findServer back = do 
                     nodes <- liftIO $ findPeers back 500
--- ChatServer.hs
+++ ChatServer.hs
@@ -8,6 +8,8 @@ import Control.Distributed.Process.Backend.SimpleLocalnet
 import Control.Distributed.Process.Closure
 import ChatClient
 
+--Setup for the server. Registers the server pid 
+--befor launching server
 master :: Backend -> [NodeId] -> Process ()
 master backend slaves = do
     self <- getSelfPid
@@ -15,6 +17,10 @@ master backend slaves = do
     liftIO $ putStrLn "Server ready!"
     server backend [] [] slaves
 
+
+--The main server function. Listens for messages and 
+--responds by spawning processes on the sending node
+--and possibly by spawning handlers on the local node.
 server :: Backend -> [(String, SendPort (String, NodeId))] -> 
             [(String, NodeId)]-> [NodeId] -> Process ()
 server backend chatRooms users slaves = do
@@ -22,11 +28,17 @@ server backend chatRooms users slaves = do
     self <- getSelfPid
     peers <- liftIO $ findPeers backend 500
     case (cmd, args) of 
+     --usr requests to start a chat with otherUsr
      ("/startChat", [usr, otherUsr]) -> 
          case findX otherUsr users of
+          --If otherUsr does not exist, inform user 
           Nothing -> let msg = "No such user."
                      in do spawn nid ($(mkClosure 'waitForInput) (msg, self))
                            server backend chatRooms users slaves
+          --If otherUsr does not exist, spawn a listening process
+          --on both usr and otherUsrs node. Then, spawn a sending
+          --process on both usr & otherUsr, with the sendPort
+          --corresponding to the other users receivePort
           Just n  -> do sPort1 <- spawnChannel $(mkStatic 'sdictString) nid 
                                                ($(mkClosure 'chatReceive) (()))
                         sPort2 <- spawnChannel $(mkStatic 'sdictString) n 
@@ -39,32 +51,51 @@ server backend chatRooms users slaves = do
                                    ("Now chatting with " ++ usr))  
                         server backend chatRooms users slaves
      ("/startRoom", [roomName, usr]) -> do 
-            sPortC <- spawnChannel $(mkStatic 'sdictString) nid 
-                                    ($(mkClosure 'chatReceive) (()))
-            sPortR <- spawnChannelLocal $ roomHandler roomName [(usr, sPortC)]
-            spawn nid ($(mkClosure 'roomSend) (usr, sPortR))
+            --Start a chatRoom by spawning a local process running
+            --the roomHandler function. Then, send a message on the
+            --channel belonging to that process, including the NodeId
+            --belonging to usr, informing it that usr wishes to join the room.
+            --Then, spawn a process on usrs node that will send messages to the
+            --room process.
             spawn nid ($(mkClosure 'serverMessage) 
-                         ("Started room " ++ roomName))
+                        ("Started room " ++ roomName))
+            sPortR <- spawnChannelLocal $ roomHandler roomName []
+            sendChan sPortR (("//addUser " ++ usr), nid)
+            spawn nid ($(mkClosure 'roomSend) (usr, sPortR))
             server backend ((roomName, sPortR):chatRooms) users slaves
      ("/joinRoom", [room, user])  -> do 
+     --Attempt to join an existing chatroom
             case findX room chatRooms of
+             --If the room does not exist, the user will be told as much
              Nothing    -> do spawn nid ($(mkClosure 'waitForInput)
                                             ("No such room", self)) 
                               server backend chatRooms users slaves
+             --If the room does exist, send a message on its channel,
+             --saying that usr wishes to connect to the room. Then,
+             --spawn a process on usrs node that will send messages
+             --to the room process.
              Just sPort -> do sendChan sPort (("//addUser " ++ user), nid)
                               spawn nid ($(mkClosure 'roomSend) (user, sPort))
                               spawn nid ($(mkClosure 'serverMessage) 
                                             ("Joined room " ++ room))
                               server backend chatRooms users slaves
-     ("/help", _)      -> do spawn nid ($(mkClosure 'waitForInput) 
+     ("/help", _)      -> --Print a message on the clients terminal,
+                          --informing him/her about the available commands
+                          do spawn nid ($(mkClosure 'waitForInput) 
                                             (helpMessage, self))
                              server backend chatRooms users slaves
-     ("/connect", _)   -> do spawn nid ($(mkClosure 'waitForInput) 
+     ("/connect", _)   -> --Spawn a process on the user that will ask for
+                          --input and forward commands to the server
+                          do spawn nid ($(mkClosure 'waitForInput) 
                                         (initMessage, self)) 
                              server backend chatRooms users slaves
-     ("/remRoom", [roomName]) -> let chatRooms' = remRoom roomName chatRooms
+     ("/remRoom", [roomName]) -> --Remove the given room from the list of
+                                 --available rooms
+                                 let chatRooms' = remRoom roomName chatRooms
                                  in  server backend chatRooms' users slaves
-     ("/passive", [usr])   -> 
+     ("/passive", [usr])   -> --Add the given user to the list of available 
+                              --users to chat with, if the chosen username
+                              --is available.
         case findX usr users of
                 Nothing -> do spawn nid ($(mkClosure 'serverMessage) 
                                ("Waiting for another user to start chat..."))
@@ -85,6 +116,10 @@ server backend chatRooms users slaves = do
           remRoom _ []          = []
           remRoom x ((y, n):ys) = if x == y then ys else (y, n):(remRoom x ys)
 
+
+--A handler for chatrooms. Listens for messages
+--and broadcasts to all connected clients, unless it's a command
+--to add/remove a client. 
 roomHandler :: String -> [(String, SendPort String)] -> 
                 ReceivePort (String, NodeId) -> Process ()
 roomHandler self clients rPort = do
@@ -93,28 +128,42 @@ roomHandler self clients rPort = do
     let msgWords = words msg' 
     case head msgWords of
         "/addUser"    -> do  
+            --Upon receiving a request to add a user,
+            --the room process will spawn a listening process
+            --on the users node and store a sendPort to said process.
+            --Then, a message is broadcast to all other users that 
+            --the user has joined the channel.
             let usr = head (drop 1 msgWords)
             sPort <- spawnChannel $(mkStatic 'sdictString) nid 
                                   ($(mkClosure 'chatReceive) (()))  
             mapM (\(_, chan) -> sendChan chan ("User " ++ usr ++ " joined!")) clients
             roomHandler self ((usr, sPort):clients) rPort
         "/disconnect" -> do 
+            --Upon receiving a disconnect request from a client,
+            --the room process will find the control process on the
+            --clients node and send a message to it, causing all
+            --processes on said node to terminate, as they are 
+            --all linked to the control process.
             let usr = takeWhile (\a -> a /= '>') msg
             let clients' = remUser usr clients
             liftIO $ putStrLn $ "Disconnecting user. Clients: " ++ (show clients')
             whereisRemoteAsync nid "control"
             WhereIsReply _ (Just con) <- expect :: Process WhereIsReply
             spawn nid ($(mkClosure 'serverMessage) ("Exiting chatroom..."))
-            send con "8==D"
+            send con "Ded"
             mapM (\(_, chan) -> 
                    sendChan chan ("User " ++ usr ++ " left")) clients'
-            if clients' == []
+            --If there are no clients left in the room after removing
+            --the requested client, then kill the room and inform the
+            --main server process to remove it from the list of available
+            --rooms.
+            if clients' == [] 
             then do nid <- getSelfNode
                     Just serv <- whereis "server"
                     send serv ("/remRoom", [self], nid)
                     return ()
             else roomHandler self (remUser usr clients') rPort
-        _            -> do 
+        _            -> do --Send the received message to all users
                          mapM (\(_, chan) -> sendChan chan msg) clients
                          roomHandler self clients rPort
   where remUser :: String -> [(String, SendPort String)] -> 
@@ -123,14 +172,6 @@ roomHandler self clients rPort = do
         remUser s ((y, sp):ys) = if s == y
                                  then ys
                                  else (y, sp):(remUser s ys)
-
-greetMessage :: String
-greetMessage = "Welcome to the Awesomely Radical chat client! \n" ++
-               "Please start by connecting to a user or chat room. \n" ++
-               "For a list of available users, enter /users \n" ++
-               "For a list of available chatrooms, enter /rooms. \n" ++
-               "For other commands, please enter /help \n \n" ++
-               "Please enter a username"
 
 initMessage :: String 
 initMessage = "Welcome to the Awesomely Radical chat client! \n" ++
